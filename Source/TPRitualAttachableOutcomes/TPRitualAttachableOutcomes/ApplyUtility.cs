9afa246a1c863438b2e70942ad1283fb763627bf
thesePeople <88640591+thesePeople@users.noreply.github.com>
2021-08-08 22:36:42 -0400

    Initial commit

---
+++ Source/TPRitualAttachableOutcomes/TPRitualAttachableOutcomes/ApplyUtility.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace TPRitualAttachableOutcomes
+{
+    static class ApplyUtility
+    {
+        public static void ApplyNode(RitualAttachableOutcomeEffectDef_TP_Custom nodeToProcess, Dictionary<Pawn, int> totalPresence, LordJob_Ritual jobRitual, OutcomeChance outcome, ref LookTargets letterLookTargets, List<Pawn> pawnsToApplyTo = null)
+        {
+            // wowee jeez I feel like this needs a refactor
+
+          //  Log.Message("somehow I'm suspicious about the totalPresence");
+           // Log.Message("totalPresence count is " + totalPresence.Count);
+
+            List<int> thisTriggerPositivityIndex = nodeToProcess.triggerPositivityIndex;
+
+            bool runThis = false;
+            if (thisTriggerPositivityIndex != null && thisTriggerPositivityIndex.Count > 0)
+            {
+                foreach (int i in thisTriggerPositivityIndex)
+                {
+                    //Log.Message("checking positivityIndex " + i);
+                    if (outcome.positivityIndex == i)
+                    {
+                        //Log.Message("foundit! running this node");
+                        runThis = true;
+                    }
+                }
+            }
+            else
+            {
+                // default to running it if a positivity index list is not supplied
+                runThis = true;
+            }
+
+            if (!runThis)
+            {
+                return;
+            }
+
+            List<string> thisAppliesTo = nodeToProcess.appliesTo;
+            bool thisAppliesToRandom = nodeToProcess.appliesToRandom;
+            bool thisInvertApply = nodeToProcess.invertApply;
+
+            string thisHediff = nodeToProcess.hediffToAdd ?? "";
+            string thisAbilityToAdd = nodeToProcess.abilityToAdd ?? "";
+            float thisHediffSeverity = nodeToProcess.hediffSeverity;
+            string thisBodyPart = nodeToProcess.bodyPart ?? "";
+            string thisHediffToRemove = nodeToProcess.hediffToRemove ?? "";
+
+            string thisThought = nodeToProcess.thought ?? "";
+            string thisInspiration = nodeToProcess.inspiration ?? "";
+
+            string thisItem = nodeToProcess.item ?? "";
+            int thisBaseAmount = nodeToProcess.baseAmount.RandomInRange;
+            int thisAmountPerPawn = nodeToProcess.amountPerPawn.RandomInRange;
+            bool thisSpawnNearRitual = nodeToProcess.spawnNearRitual;
+
+            string thisWeather = nodeToProcess.weather ?? "";
+
+            string thisLetterLabel = nodeToProcess.letterLabel ?? "";
+            string thisLetterText = nodeToProcess.letterText ?? "";
+            string thisLetterType = nodeToProcess.letterType ?? "";
+
+            bool thisRandomfromNode = nodeToProcess.randomFromNode;
+            bool thisForEachPawn = nodeToProcess.forEachPawn;
+
+            List<RitualAttachableOutcomeEffectDef_TP_Custom_Node> subNodes = nodeToProcess.node;
+
+           // Log.Message("Finished loading in the outcome effect data");
+
+
+            bool thisTargetLetter = false;
+            Map map = totalPresence.First().Key.Map;
+            IntVec3 intVec = DropCellFinder.RandomDropSpot(map);
+            if(thisSpawnNearRitual)
+            {
+                intVec = jobRitual.selectedTarget.Cell;
+            }
+            if (pawnsToApplyTo == null)
+            {
+                pawnsToApplyTo = new List<Pawn>();
+                if (thisInvertApply)
+                {
+                    // this is going to look redundant and yes, could probably be refactored. But it also provides more graceful fallback
+                    if (thisAppliesTo != null && thisAppliesTo.Count > 0)
+                    {
+                        pawnsToApplyTo = totalPresence.Keys.ToList<Pawn>();
+
+                        foreach (string s in thisAppliesTo)
+                        {
+                           // Log.Message("Removing " + s + " to the apply to list");
+                            if (jobRitual.PawnWithRole(s) != null)
+                            {
+                                pawnsToApplyTo.Remove(jobRitual.PawnWithRole(s));
+                            }
+                        }
+                    }
+
+
+                }
+                else if (thisAppliesTo != null && thisAppliesTo.Count > 0)
+                {
+                    //Log.Message("checking who to apply the outcome to");
+                    // if they've supplied a list of roleIds to apply to, get those
+                    foreach (string s in thisAppliesTo)
+                    {
+                        //Log.Message("Adding " + s + " to the apply to list");
+                        if (jobRitual.PawnWithRole(s) != null)
+                        {
+                            pawnsToApplyTo.Add(jobRitual.PawnWithRole(s));
+                        }
+                    }
+                }
+                else
+                {
+                   // Log.Message("Adding everyone involved to the apply to list");
+                    // they didn't supply a list, so we apply to everyone involved
+                    pawnsToApplyTo = totalPresence.Keys.ToList<Pawn>();
+                }
+
+                if (thisAppliesToRandom)
+                {
+                    // if they set the flag that it should be a random pawn it'll pick a random pawn from the valid list of appliesTo
+                    // if they don't include the appliesTo list, it'll be a random pawn from the totalPresence
+                    List<Pawn> tmpList = new List<Pawn>();
+                    tmpList.Add(pawnsToApplyTo.RandomElement<Pawn>());
+                    pawnsToApplyTo = tmpList;
+
+                    // TODO refactor this ;)
+                }
+            }
+
+           // Log.Message("Iterating through pawns to apply to");
+            foreach (Pawn pawn in pawnsToApplyTo)
+            {
+               // Log.Message("Pawn " + pawn.Name + " is actually in the list");
+
+                // now if they add a hediff it only gets added to pawns obeying the whole "appliesTo" thing
+                // I feel like this whole thing would be a lot shorter if I remembered LINQ better
+
+               // Log.Message("thisHediff is " + thisHediff + ". If you see a blank space then something's wrong");
+                if (!String.IsNullOrEmpty(thisHediff))
+                {
+                    
+                   //  Log.Message("Applying " + thisHediff + " to pawn...");
+                    Hediff hediffToAdd = HediffMaker.MakeHediff(DefDatabase<HediffDef>.GetNamed(thisHediff), pawn, null);
+
+                    BodyPartRecord bpr = new BodyPartRecord();
+                    if (!String.IsNullOrEmpty(thisBodyPart))
+                    {
+                        bpr = pawn.RaceProps.body.GetPartsWithDef(DefDatabase<BodyPartDef>.GetNamed(thisBodyPart)).First();
+                        pawn.health.AddHediff(hediffToAdd, bpr, null, null);
+                    }
+                    else    
+                    {
+                        pawn.health.AddHediff(hediffToAdd);
+                    }
+                    
+                    if (thisHediffSeverity > 0)
+                    {
+                        List<Hediff> tmpHediffs = new List<Hediff>();
+                        tmpHediffs.AddRange(pawn.health.hediffSet.hediffs);
+                        for (int i = 0; i < tmpHediffs.Count; i++)
+                        {
+                            Hediff tmpHediff = tmpHediffs[i];
+
+                            if (tmpHediff.def.defName == thisHediff)
+                            {
+                                tmpHediff.Severity += thisHediffSeverity;
+                            }
+                        }
+                    }
+                }
+
+                if (!String.IsNullOrEmpty(thisAbilityToAdd))
+                {
+                    // Log.Message("Applying " + thisAbilityToAdd + " to pawn...");
+                    pawn.abilities.GainAbility(DefDatabase<AbilityDef>.GetNamed(thisAbilityToAdd));
+                }
+
+                if (!String.IsNullOrEmpty(thisHediffToRemove))
+                {
+                    List<Hediff> tmpHediffs = new List<Hediff>();
+                    tmpHediffs.AddRange(pawn.health.hediffSet.hediffs);
+                    for(int i = 0; i < tmpHediffs.Count; i++)
+                    {
+                        Hediff tmpHediff = tmpHediffs[i];
+
+                        if(tmpHediff.def.defName == thisHediffToRemove)
+                        {
+                            pawn.health.RemoveHediff(tmpHediff);
+                        }
+                    }
+                    
+                }
+
+                if(!String.IsNullOrEmpty(thisThought))
+                {
+                    pawn.needs.mood.thoughts.memories.TryGainMemory(DefDatabase<ThoughtDef>.GetNamed(thisThought));
+                }
+
+                if (!String.IsNullOrEmpty(thisInspiration))
+                {
+                    pawn.mindState.inspirationHandler.TryStartInspiration(DefDatabase<InspirationDef>.GetNamed(thisInspiration));
+                }
+            }
+
+            // if they want to spawn items
+            if (!String.IsNullOrEmpty(thisItem))
+            {
+                // get the things to add to the list
+
+                List<Thing> things = new List<Thing>();
+                int numPawns = totalPresence.Count();
+
+                int remainingAmount = thisBaseAmount + thisAmountPerPawn * numPawns;
+
+                while (remainingAmount > 0)
+                {
+                    Thing thing = ThingMaker.MakeThing(DefDatabase<ThingDef>.GetNamed(thisItem));
+                    if (remainingAmount > thing.def.stackLimit)
+                    {
+                        remainingAmount -= thing.def.stackLimit;
+                        thing.stackCount = thing.def.stackLimit;
+                    }
+                    else
+                    {
+                        thing.stackCount = remainingAmount;
+                        remainingAmount = 0;
+                    }
+                    things.Add(thing);
+                }
+
+                thisTargetLetter = true;
+
+                // actually drop the things
+                DropPodUtility.DropThingsNear(intVec, map, things, 110, canInstaDropDuringInit: false, leaveSlag: false, canRoofPunch: false);
+            }
+
+            // weather?
+            if (!String.IsNullOrEmpty(thisWeather))
+            {
+                Find.CurrentMap.weatherManager.TransitionTo(DefDatabase<WeatherDef>.GetNamed(thisWeather));
+            }
+
+            // force other event?
+
+            // send a letter
+            LetterDef letterType = LetterDefOf.PositiveEvent;
+
+            //since thisLetterType is a string and not a String we should be able to do this
+            if (thisLetterType == "neutral")
+            {
+                letterType = LetterDefOf.NeutralEvent;
+            }
+            else if (thisLetterType == "negative")
+            {
+                letterType = LetterDefOf.NegativeEvent;
+            }
+
+            if (!String.IsNullOrEmpty(thisLetterLabel))
+            {
+                if (thisTargetLetter)
+                {
+                    Find.LetterStack.ReceiveLetter(LetterMaker.MakeLetter(new TaggedString(thisLetterLabel), new TaggedString(thisLetterText), letterType, new TargetInfo(intVec, map)));
+                }
+                else
+                {
+                    Find.LetterStack.ReceiveLetter(LetterMaker.MakeLetter(new TaggedString(thisLetterLabel), new TaggedString(thisLetterText), letterType));
+                }
+            }
+
+            // apply subnodes
+            if (subNodes != null && subNodes.Count > 0)
+            {
+                if (thisForEachPawn)
+                {
+                    foreach (Pawn p in pawnsToApplyTo)
+                    {
+                        List<Pawn> tmpPawnList = new List<Pawn>();
+                        tmpPawnList.Add(p);
+                        if (thisRandomfromNode)
+                        {
+                            ApplyNode(subNodes.RandomElementByWeight((RitualAttachableOutcomeEffectDef_TP_Custom_Node r) => r.weight), totalPresence, jobRitual, outcome, ref letterLookTargets, tmpPawnList);
+                        }
+                        else
+                        {
+                            foreach (RitualAttachableOutcomeEffectDef_TP_Custom_Node node in subNodes)
+                            {
+                                ApplyNode(node, totalPresence, jobRitual, outcome, ref letterLookTargets, tmpPawnList);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+
+                    if (thisRandomfromNode)
+                    {
+                        ApplyNode(subNodes.RandomElementByWeight((RitualAttachableOutcomeEffectDef_TP_Custom_Node r) => r.weight), totalPresence, jobRitual, outcome, ref letterLookTargets);
+                    }
+                    else
+                    {
+                        foreach (RitualAttachableOutcomeEffectDef_TP_Custom_Node node in subNodes)
+                        {
+                            ApplyNode(node, totalPresence, jobRitual, outcome, ref letterLookTargets);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Source/TPRitualAttachableOutcomes/TPRitualAttachableOutcomes/HarmonyPatcher.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+using UnityEngine;
+
+
+namespace TPRitualAttachableOutcomes
+{
+    [StaticConstructorOnStartup]
+    static class HarmonyPatcher
+    {
+
+        static HarmonyPatcher()
+        {
+            Harmony harmony = new Harmony("TPRitualAttachableOutcomes.HarmonyPatches");
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(RitualOutcomeEffectWorker_FromQuality))]
+    [HarmonyPatch("ApplyAttachableOutcome")]
+    public static class Patch_RitualOutcomeEffectWorker_FromQuality
+    {
+        public static void Prefix(Dictionary<Pawn, int> totalPresence, LordJob_Ritual jobRitual, OutcomeChance outcomeChance,  string extraLetterText, ref LookTargets letterLookTargets)
+        {
+            if(jobRitual == null || jobRitual.Ritual == null || jobRitual.Ritual.attachableOutcomeEffect == null || jobRitual.Ritual.attachableOutcomeEffect.GetModExtension<RitualAttachableOutcomeEffectDef_TP_Custom>() == null)
+            {
+                return;
+            }
+
+            RitualAttachableOutcomeEffectDef_TP_Custom customRitualAttachableOutcomeEffect = jobRitual.Ritual.attachableOutcomeEffect.GetModExtension<RitualAttachableOutcomeEffectDef_TP_Custom>();
+            if(customRitualAttachableOutcomeEffect != null)
+            {
+                jobRitual.Ritual.attachableOutcomeEffect.onlyPositiveOutcomes = customRitualAttachableOutcomeEffect.onlyPositiveOutcomes;
+            }
+
+            return;
+        }
+    }
+
+    [HarmonyPatch(typeof(Precept_Ritual), "get_RepeatPenaltyProgress")]
+    internal class Patch_RepeatPenaltyProgress
+    {
+        private static bool Prefix(Precept_Ritual __instance, ref float __result)
+        {
+            int coolDownDays = 20;
+            Precept_Ritual_Custom customPreceptRitual = __instance.def.GetModExtension<Precept_Ritual_Custom>();
+            if(customPreceptRitual != null)
+            {
+                //Log.Message("customCooldown: " + customPreceptRitual.coolDownDays);
+                if (customPreceptRitual.coolDownDays != coolDownDays)
+                {
+                    coolDownDays = customPreceptRitual.coolDownDays;
+                }
+            }
+            __result = (float)__instance.TicksSinceLastPerformed / (60000f * (float)coolDownDays);
+            //Log.Message("RepeatPenaltyProgress: " + __result);
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Precept_Ritual), "get_RepeatPenaltyTimeLeft")]
+    internal class Patch_RepeatPenaltyTimeLeft
+    {
+        private static bool Prefix(Precept_Ritual __instance, ref string __result)
+        {
+            int coolDownDays = 20;
+            Precept_Ritual_Custom customPreceptRitual = __instance.def.GetModExtension<Precept_Ritual_Custom>();
+            if (customPreceptRitual != null)
+            {
+                //Log.Message("customCooldown: " + customPreceptRitual.coolDownDays);
+                if (customPreceptRitual.coolDownDays != 20)
+                {
+                    coolDownDays = customPreceptRitual.coolDownDays;
+                }
+            }
+            int myTicks = 60000 * coolDownDays;
+            __result = (myTicks - __instance.TicksSinceLastPerformed).ToStringTicksToPeriod();
+            //Log.Message("RepeatPenaltyTimeLeft: " + __result);
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Precept_Ritual), "get_RepeatPenaltyActive")]
+    internal class Patch_RepeatPenaltyActive
+    {
+        private static bool Prefix(Precept_Ritual __instance, ref bool __result)
+        {
+            int coolDownDays = 20;
+            Precept_Ritual_Custom customPreceptRitual = __instance.def.GetModExtension<Precept_Ritual_Custom>();
+            if (customPreceptRitual != null)
+            {
+                //Log.Message("customCooldown: " + customPreceptRitual.coolDownDays);
+                if (customPreceptRitual.coolDownDays != 20)
+                {
+                    coolDownDays = customPreceptRitual.coolDownDays;
+                }
+            }
+            int myTicks = 60000 * coolDownDays;
+            if (__instance.isAnytime && __instance.lastFinishedTick != -1 && __instance.def.useRepeatPenalty)
+            {
+                __result = __instance.TicksSinceLastPerformed < myTicks;
+                //Log.Message("RitualPenaltyActive: " + __result);
+            } else
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    //RepeatQualityPenalty
+    [HarmonyPatch(typeof(Precept_Ritual), "get_RepeatQualityPenalty")]
+    internal class Patch_RepeatQualityPenalty
+    {
+        private static bool Prefix(Precept_Ritual __instance, ref float __result)
+        {
+            int coolDownDays = 20;
+            Precept_Ritual_Custom customPreceptRitual = __instance.def.GetModExtension<Precept_Ritual_Custom>();
+               
+           // Log.Message("getting the real cooldown for " + __instance.def.defName);
+            
+            if (customPreceptRitual != null)
+            {
+                //Log.Message("customCooldown: " + customPreceptRitual.coolDownDays + " for ritual " + __instance.def.label); 
+                if (customPreceptRitual.coolDownDays != coolDownDays)
+                {
+                    coolDownDays = customPreceptRitual.coolDownDays;
+                }
+            }
+            float penaltyProgress = (float)__instance.TicksSinceLastPerformed / (60000f * (float)coolDownDays); 
+            __result = Mathf.Lerp(-0.95f, 0f, penaltyProgress);
+            float origResult = Mathf.Lerp(-0.95f, 0f, (float)__instance.TicksSinceLastPerformed / (60000f * 20f));
+            //Log.Message("RepeatQualityPenalty: " + __result + " (instead of " + origResult + ")");
+            return false;
+        }
+    }
+
+   
+    // TipMainPart
+    [HarmonyPatch(typeof(Precept_Ritual), "TipMainPart")]
+    internal class Patch_TipMainPart
+    {
+        private static string ColorizeWarning(TaggedString title)
+        {
+            return title.Resolve().Colorize(ColoredText.ThreatColor);
+        }
+
+        private static bool Prefix(Precept_Ritual __instance, ref string __result)
+        {
+            int coolDownDays = 20;
+            Precept_Ritual_Custom customPreceptRitual = __instance.def.GetModExtension<Precept_Ritual_Custom>();
+
+            //Log.Message("getting the real cooldown for " + __instance.def.defName);
+
+            if (customPreceptRitual != null)
+            {
+                //Log.Message("customCooldown: " + customPreceptRitual.coolDownDays + " for ritual " + __instance.def.label);
+                if (customPreceptRitual.coolDownDays != coolDownDays)
+                {
+                    coolDownDays = customPreceptRitual.coolDownDays;
+                } 
+                else // so far the only reason to override this is for a non-default cooldown, so if it is the default cooldown just go back to the original
+                {
+                    return true;
+                }
+            }
+            float penaltyProgress = (float)__instance.TicksSinceLastPerformed / (60000f * (float)coolDownDays);
+
+            StringBuilder tmpCompsDesc = new StringBuilder();
+            
+            if (__instance.RepeatPenaltyActive)
+            {
+                float num = (float)Mathf.RoundToInt(__instance.RepeatPenaltyProgress * (float)coolDownDays * 10f) / 10f;
+                float num2 = (float)Mathf.RoundToInt((1f - __instance.RepeatPenaltyProgress) * (float)coolDownDays * 10f) / 10f;
+                tmpCompsDesc.AppendLine(ColorizeWarning("RitualRepeatPenaltyTip".Translate(coolDownDays, num, __instance.RepeatQualityPenalty.ToStringPercent(), num2)));
+                tmpCompsDesc.AppendLine();
+            }
+            if (!__instance.Description.NullOrEmpty())
+            {
+                tmpCompsDesc.Append(__instance.Description);
+            }
+            if (__instance.outcomeEffect != null)
+            {
+                StringBuilder stringBuilder = new StringBuilder(__instance.outcomeEffect.def.Description);
+                if (!__instance.outcomeEffect.def.extraPredictedOutcomeDescriptions.NullOrEmpty())
+                {
+                    foreach (string extraPredictedOutcomeDescription in __instance.outcomeEffect.def.extraPredictedOutcomeDescriptions)
+                    {
+                        stringBuilder.Append(" " + extraPredictedOutcomeDescription.Formatted(__instance.shortDescOverride ?? __instance.def.label));
+                    }
+                }
+                if (__instance.attachableOutcomeEffect != null)
+                {
+                    if (stringBuilder.Length > 0)
+                    {
+                        stringBuilder.AppendLine();
+                    }
+                    stringBuilder.AppendInNewLine(__instance.attachableOutcomeEffect.DescriptionForRitualValidated(__instance));
+                }
+                if (stringBuilder.Length > 0)
+                {
+                    tmpCompsDesc.AppendLine();
+                    tmpCompsDesc.AppendInNewLine(stringBuilder.ToString());
+                }
+            }
+            __result = tmpCompsDesc.ToString();
+            return false;
+        }
+    }
+
+    /*[HarmonyPatch(typeof(Precept_Ritual), "get_RepeatPenaltyDurationDays")]
+    internal class Patch_RepeatPenaltyDurationDays
+    {
+        private static bool Prefix(Precept_Ritual __instance, ref int __result)
+        {
+            int coolDownDays = 20;
+            Precept_Ritual_Custom customPreceptRitual = __instance.def.GetModExtension<Precept_Ritual_Custom>();
+            if (customPreceptRitual != null)
+            {
+                if (customPreceptRitual.coolDownDays != 20)
+                {
+                    coolDownDays = customPreceptRitual.coolDownDays;
+                }
+            }
+            __result = coolDownDays;
+            return false;
+        }
+    }*/
+}
