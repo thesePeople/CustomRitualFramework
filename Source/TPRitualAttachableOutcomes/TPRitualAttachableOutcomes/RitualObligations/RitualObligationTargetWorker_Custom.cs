d61c2699aab67456a492ab2cddedad014cf58a5c
thesePeople <michael.kreuter@gmail.com>
2021-08-21 15:36:24 -0400

    Pure custom ritual target and obligation filters

---
+++ Source/TPRitualAttachableOutcomes/TPRitualAttachableOutcomes/RitualObligations/RitualObligationTargetWorker_Custom.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace TPRitualAttachableOutcomes
+{
+    class RitualObligationTargetWorker_Custom : RitualObligationTargetFilter
+    {
+		public RitualObligationTargetWorker_Custom()
+		{
+		}
+
+		public RitualObligationTargetWorker_Custom(RitualObligationTargetFilterDef def)
+			: base(def)
+		{
+		}
+
+		public override IEnumerable<TargetInfo> GetTargets(RitualObligation obligation, Map map)
+		{
+			if (!ModLister.CheckIdeology("Execution target"))
+			{
+				yield break;
+			}
+			if (def != null && def.GetModExtension<TargetFilter_ModExtension>() != null)
+			{
+				TargetFilter_ModExtension modExtension = def.GetModExtension<TargetFilter_ModExtension>();
+				foreach (string s in modExtension.extraCandidates)
+				{
+					List<Thing> newSpots = map.listerThings.ThingsOfDef(DefDatabase<ThingDef>.GetNamed(s));
+					for (int j = 0; j < newSpots.Count; j++)
+					{
+						yield return newSpots[j];
+					}
+				}
+			}
+		}
+
+		protected override RitualTargetUseReport CanUseTargetInternal(TargetInfo target, RitualObligation obligation)
+		{
+			if (!target.HasThing)
+			{
+				return false;
+			}
+			if (def != null && def.GetModExtension<TargetFilter_ModExtension>() != null)
+			{
+				TargetFilter_ModExtension modExtension = def.GetModExtension<TargetFilter_ModExtension>();
+				foreach (string s in modExtension.extraCandidates)
+				{
+					if (target.Thing.def.defName == s)
+					{
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+
+		public override IEnumerable<string> GetTargetInfos(RitualObligation obligation)
+		{
+			if (def != null && def.GetModExtension<TargetFilter_ModExtension>() != null)
+			{
+				TargetFilter_ModExtension modExtension = def.GetModExtension<TargetFilter_ModExtension>();
+				foreach (string s in modExtension.extraCandidates)
+				{
+					yield return s;
+				}
+			}
+			else
+            {
+				yield return null;
+            }
+		}
+
+	}
+}
+
---
+++ Source/TPRitualAttachableOutcomes/TPRitualAttachableOutcomes/RitualTargetFilters/RitualTargetFilter_Custom.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+using Verse.AI;
+
+namespace TPRitualAttachableOutcomes
+{
+    class RitualTargetFilter_Custom : RitualTargetFilter
+    {
+		public RitualTargetFilter_Custom()
+		{
+		}
+
+		public RitualTargetFilter_Custom(RitualTargetFilterDef def)
+			: base(def)
+		{
+		}
+
+		public override bool CanStart(TargetInfo initiator, TargetInfo selectedTarget, out string rejectionReason)
+		{
+			TargetInfo targetInfo = BestTarget(initiator, selectedTarget);
+			rejectionReason = "";
+			if (!targetInfo.IsValid)
+			{
+				rejectionReason = "RequiredBuildingsNotFound".Translate();
+				return false;
+			}
+			return true;
+		}
+
+		public override TargetInfo BestTarget(TargetInfo initiator, TargetInfo selectedTarget)
+		{
+			Pawn pawn = initiator.Thing as Pawn;
+			if (pawn == null)
+			{
+				return null;
+			}
+			List<Building> buildingList = new List<Building>();
+
+			TargetFilter_ModExtension modExtension = def.GetModExtension<TargetFilter_ModExtension>();
+			if (modExtension.extraCandidates != null && modExtension.extraCandidates.Count > 0)
+            {
+				foreach(string s in modExtension.extraCandidates)
+                {
+					buildingList.AddRange(initiator.Thing.Map.listerBuildings.AllBuildingsColonistOfDef(DefDatabase<ThingDef>.GetNamed(s)));
+                }
+            }
+            else
+            {
+				return null;
+            }
+
+			Thing thing = null;
+			float num = 99999f;
+			
+			foreach (Building item2 in buildingList)
+			{
+				if (item2.def.isAltar && pawn.CanReach(item2, PathEndMode.Touch, pawn.NormalMaxDanger()))
+				{
+					int lengthHorizontalSquared = (pawn.Position - item2.Position).LengthHorizontalSquared;
+					if ((float)lengthHorizontalSquared < num)
+					{
+						thing = item2;
+						num = lengthHorizontalSquared;
+					}
+				}
+			}
+			
+			if (thing == null && def.fallbackToRitualSpot)
+			{
+				foreach (Thing item3 in pawn.Map.listerThings.ThingsOfDef(ThingDefOf.RitualSpot))
+				{
+					if (pawn.CanReach(item3, PathEndMode.Touch, pawn.NormalMaxDanger()))
+					{
+						int lengthHorizontalSquared2 = (pawn.Position - item3.Position).LengthHorizontalSquared;
+						if ((float)lengthHorizontalSquared2 < num)
+						{
+							thing = item3;
+							num = lengthHorizontalSquared2;
+						}
+					}
+				}
+			}
+			return thing;
+		}
+
+		public override IEnumerable<string> GetTargetInfos(TargetInfo initiator)
+		{
+			List<Building> buildingList = new List<Building>();
+
+			TargetFilter_ModExtension modExtension = def.GetModExtension<TargetFilter_ModExtension>();
+			if (modExtension.extraCandidates != null && modExtension.extraCandidates.Count > 0)
+			{
+				foreach (string s in modExtension.extraCandidates)
+				{
+					buildingList.AddRange(initiator.Thing.Map.listerBuildings.AllBuildingsColonistOfDef(DefDatabase<ThingDef>.GetNamed(s)));
+				}
+				foreach (Building b in buildingList)
+				{
+					yield return b.LabelCap;
+				}
+			}
+			else
+            {
+				yield return null;
+            }
+			
+		}
+	}
+}
+
