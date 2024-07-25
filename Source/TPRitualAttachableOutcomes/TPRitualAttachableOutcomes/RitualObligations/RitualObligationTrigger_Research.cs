8162bfc18d3aab85953817214347143c8a2d6e4f
Varnaris <elouan.vuichard@gmail.com>
2024-07-25 14:25:10 +0200

    RitualObligationTrigger_Research

---
+++ Source/TPRitualAttachableOutcomes/TPRitualAttachableOutcomes/RitualObligations/RitualObligationTrigger_Research.cs
@@ -0,0 +1,37 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace TPRitualAttachableOutcomes
+{
+    class RitualObligationTrigger_Research : RitualObligationTrigger
+    {
+
+        public string researchDefName = "";
+
+        private RitualObligation ro = null;
+
+        public override void Init(RitualObligationTriggerProperties props)
+        {
+            base.Init(props);
+            researchDefName = ((RitualObligationTrigger_ResearchProperties)props).researchDefName;
+        }
+        public void Notify_Research(ResearchProjectDef research)
+        {
+            if (this.researchDefName == research.defName && (ritual.activeObligations == null || ritual.activeObligations.Count == 0))
+            {
+                ro = new RitualObligation(ritual);
+                ritual.AddObligation(ro);
+            }
+        }
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref researchDefName, "researchDefName");
+            base.ExposeData();
+        }
+    }
+}
---
+++ Source/TPRitualAttachableOutcomes/TPRitualAttachableOutcomes/RitualObligations/RitualObligationTrigger_ResearchProperties.cs
@@ -0,0 +1,24 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace TPRitualAttachableOutcomes
+{
+    class RitualObligationTrigger_ResearchProperties : RitualObligationTriggerProperties
+    {
+        public string researchDefName = "";
+        public RitualObligationTrigger_ResearchProperties()
+        {
+            triggerClass = typeof(RitualObligationTrigger_Research);
+        }
+
+        public void ExposeData()
+        {
+            Scribe_Values.Look(ref researchDefName, "researchDefName");
+        }
+    }
+}
